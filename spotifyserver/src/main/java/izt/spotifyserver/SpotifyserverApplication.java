2e095b91bff082105ff7e6de3c4ab6d7e58e1eb2
Isaac Tee <iztyj.work@gmail.com>
2024-03-02 18:28:57 +0800

    initial commit

---
+++ spotifyserver/src/main/java/izt/spotifyserver/SpotifyserverApplication.java
@@ -0,0 +1,13 @@
+package izt.spotifyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyserverApplication.class, args);
+	}
+
+}
---
+++ spotifyserver/src/test/java/izt/spotifyserver/SpotifyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package izt.spotifyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
