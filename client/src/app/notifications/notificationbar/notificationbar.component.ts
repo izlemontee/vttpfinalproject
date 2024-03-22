820d7b040aa0aa18f7ae0204559cbfb221c405e6
Isaac Tee <iztyj.work@gmail.com>
2024-03-22 17:48:10 +0800

    added notification bar, can establish websocket connection. have not included receiving new notifs in real time

--- client/src/app/notifications/notificationbar/notificationbar.component.ts
+++ client/src/app/notifications/notificationbar/notificationbar.component.ts
@@ -1,10 +1,106 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectAllUsers } from '../../state/state.selectors';
+
+
+export const TYPE_SESSION_USERNAME = "session_username"
 
 @Component({
   selector: 'app-notificationbar',
   templateUrl: './notificationbar.component.html',
   styleUrl: './notificationbar.component.css'
 })
-export class NotificationbarComponent {
+export class NotificationbarComponent implements OnInit{
 
+  webSocketUrl: string = "ws://localhost:8080/notif-websocket"
+
+  myUsername!:string
+
+  ws!:WebSocket
+  private cdr = inject(ChangeDetectorRef)
+  private store = inject(Store)
+
+  ngOnInit(): void {
+      this.getUsernameFromStore()
+  }
+
+  getUsernameFromStore(){
+    this.store.select(selectAllUsers).subscribe({
+      next:(response)=>{
+        this.myUsername = response.username
+        if(response.username !=''){
+          console.log("connected")
+          this.connectToWebsocket()
+        }else{
+          this.disconnectToWebSocket()
+        }
+       
+      }
+    })
+  }
+
+  connectToWebsocket(){
+    if("WebSocket" in window){
+      this.ws = new WebSocket(this.webSocketUrl)
+      this.ws.onopen = this.onWebSocketOpen.bind(this)
+      this.ws.onmessage = this.onWebSocketMessage.bind(this)
+      this.ws.onmessage = this.onReceive.bind(this)
+      this.ws.onclose = this.onWebSocketClose.bind(this)
+
+      this.ws.onerror = (error) =>{
+        console.error("Websocket error", error)
+    }
+  }
+}
+  // using the native websocket library
+  onWebSocketOpen(){
+    console.log("websocket connected in app component")
+    this.sendUsernameToServer()
+
+  }
+
+  onWebSocketClose(){
+    console.log('websocket closed')
+  }
+
+  onWebSocketMessage(evt: {data:string}){
+    let received_msg = JSON.parse(evt.data)
+    console.log("received_msg", received_msg)
+  }
+
+  onReceive(event:MessageEvent<any>){
+    try{
+        let receivedMsg = JSON.parse(event.data)
+        this.cdr.detectChanges()
+
+
+    }catch(error){
+      console.error("error parsing message ", error)
+    }
+
+  }
+
+  disconnectToWebSocket(){
+    if(this.ws){
+      this.ws.close()
+      console.log("closed")
+    }else(
+      console.error("No websocket connection")
+    )
+  }
+
+  sendUsernameToServer(){
+    if(this.ws && this.ws.readyState === WebSocket.OPEN){
+      const payload={
+        type:TYPE_SESSION_USERNAME,
+        username:this.myUsername
+      }
+
+      this.ws.send(JSON.stringify(payload))
+      console.log("send username success")
+    }
+    else{
+      console.error("failed to send message. might still be connecting")
+    }
+  }
 }
--- spotifyserver/src/main/java/izt/spotifyserver/config/WebSocketHandler.java
+++ spotifyserver/src/main/java/izt/spotifyserver/config/WebSocketHandler.java
@@ -1,9 +1,64 @@
 package izt.spotifyserver.config;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import izt.spotifyserver.Utils.Utils;
+import jakarta.json.JsonObject;
 
 @Component
 public class WebSocketHandler extends TextWebSocketHandler{
+
+    private final String TYPE_SESSION_USERNAME = "session_username";
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketHandler.class);
+
+    Map<String, List<WebSocketSession>> sessions = new HashMap<>();
     
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message){
+        JsonObject payload = Utils.stringToJson(message.getPayload().toString());
+        String type = payload.getString("type");
+        switch(type){
+            case TYPE_SESSION_USERNAME:{
+                String username = payload.getString("username");
+                addToSessions(username, session);
+                break;
+            }
+        }
+        
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status){
+
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session){
+        String origin = session.getHandshakeHeaders().getFirst("Origin");
+        System.out.println("id: "+session.getId());
+        logger.info("WebSocket connection established from origin: {}", origin);
+    }
+
+    public void addToSessions(String username, WebSocketSession session){
+        if(sessions.containsKey(username)){
+            sessions.get(username).add(session);
+        }else{
+            List<WebSocketSession> list = new ArrayList<>();
+            list.add(session);
+            sessions.put(username, list);
+        }
+
+    }
 }
