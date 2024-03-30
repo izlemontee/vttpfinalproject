62b46f7835f2ae3bd1b06ec5f25c29ec45604f76
Isaac Tee <iztyj.work@gmail.com>
2024-03-30 23:04:17 +0800

    added posting component, have not added to database

--- client/src/app/post/post.component.ts
+++ client/src/app/post/post.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
-export class PostComponent {
+export class PostComponent implements OnInit{
 
+
+  private fb = inject(FormBuilder)
+
+  postInput!: FormGroup
+
+  ngOnInit(): void {
+      this.postInput = this.createForm()
+  }
+
+  createForm(){
+    return this.fb.group({
+      content:this.fb.control<string>('',[Validators.required, Validators.maxLength(1024)])
+    })
+  }
+
+  processForm(){
+    var content = this.postInput.value['content']
+    console.log(content)
+  }
 }
